4db12baeb088c5746496f9a5c888d7bdcc5e6534
JalajYadav <jalajyadav99@gmail.com>
2025-10-22 22:54:59 +0530

    feat(package_b): import the file and use component import the button component and it's css

--- packages/package_b/src/App.tsx
+++ packages/package_b/src/App.tsx
@@ -1,10 +1,12 @@
+import { Button } from '@lerna_monorepo/package_a';
+import '@lerna_monorepo/package_a/dist/package_a.css';
 
 function App() {
 
   return (
     <>
       <h1>This is the consumer project</h1>
-
+      <Button />
     </>
   )
 }
